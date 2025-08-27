4993d9d1154879a3335f3b72648f1339d9bf7d52
SpMandali108 <sdhmandali80@gmail.com>
2025-08-27 09:47:33 +0530

    Bill 6.0

--- website/auth.py
+++ website/auth.py
@@ -649,9 +649,9 @@ def download_customer():
            
 
             if code.startswith("K"):
-                img_path = os.path.join(current_app.static_folder, "kediya", f"{code}.webp")
+                img_path = os.path.join(current_app.static_folder, "Kediya", f"{code}.webp")
             elif code.startswith("C"):
-                img_path = os.path.join(current_app.static_folder, "choli", f"{code}.webp")
+                img_path = os.path.join(current_app.static_folder, "Choli", f"{code}.webp")
 
 
             if img_path and os.path.exists(img_path):
